a2ea684b669fe3461aa1d3a475c82de36670d516
dujthep <dujthep@dujtheps-MacBook-Pro.local>
2019-02-17 22:52:20 +0700

    Add boostrap and component serach and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AlertModule } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { FeedListComponent } from './feed-list/feed-list.component';
+import { FeedSearchComponent } from './feed-search/feed-search.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    FeedListComponent,
+    FeedSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AlertModule.forRoot()
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
